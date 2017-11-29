374748a66e946f8fa5b369004e85a081ca274a1a
sagar <sagar_gorasiya@yahoo.co.in>
2017-11-29 09:40:05 +0530

    save

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  template:'this is About Page',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number=4;
+  btnText:string='Add an Item';
+  goalText:string="My Fist Life Goal";
+goals=[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount= this.goals.length;
+  }
+  additem(){
+    alert();
+     this.goals.push(this.goalText);
+     this.goalText='';
+     this.itemCount=this.goals.length;
+}
+}
