54579962b1802ce4e9a0719f0071e54908648ba3
sagar <sagar_gorasiya@yahoo.co.in>
2017-12-01 09:57:47 +0530

    first commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,6 +8,7 @@ import { DataService } from '../data.service';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  goals:any;
 
   constructor(private route:ActivatedRoute, private router: Router, private _data: DataService) {
     this.route.params.subscribe(res=>console.log(res.id));
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,16 +34,27 @@ export class HomeComponent implements OnInit {
   itemCount: number=4;
   btnText:string='Add an Item';
   goalText:string="My Fist Life Goal";
-  goals=['one','two','three','four','five'];
-
+  goals=[];
+  records: Array<any>;
   constructor(private _data:DataService) {   
   }
 
   ngOnInit() {
-    //this.itemCount= this.goals.length;
-    this._data.goal.subscribe(res=>this.goals=res);
+    this._data.goal.subscribe(res => this.goals = res);    
     this.itemCount= this.goals.length;
     this._data.changeGoal(this.goals);
+    this.records= [
+      { CategoryID: 1,  CategoryName: "Beverages", Description: "Coffees, teas" },
+      { CategoryID: 2,  CategoryName: "Condiments", Description: "Sweet and savory sauces" },
+      { CategoryID: 3,  CategoryName: "Confections", Description: "Desserts and candies" },
+      { CategoryID: 4,  CategoryName: "Cheeses",  Description: "Smetana, Quark and Cheddar Cheese" },
+      { CategoryID: 5,  CategoryName: "Grains/Cereals", Description: "Breads, crackers, pasta, and cereal" },
+      { CategoryID: 6,  CategoryName: "Beverages", Description: "Beers, and ales" },
+      { CategoryID: 7,  CategoryName: "Condiments", Description: "Selishes, spreads, and seasonings" },
+      { CategoryID: 8,  CategoryName: "Confections", Description: "Sweet breads" },
+      { CategoryID: 9,  CategoryName: "Cheeses",  Description: "Cheese Burger" },
+      { CategoryID: 10, CategoryName: "Grains/Cereals", Description: "Breads, crackers, pasta, and cereal" }
+     ];
   }
   additem(){    
      
